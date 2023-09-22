a91027d0957b22aecb3b85a6af9962b5874a34d0
DougKF <douglaskforbes@gmail.com>
2023-09-22 11:55:21 +0100

    Completed milestone 2, completed sprint 1

---
+++ main.js
@@ -0,0 +1,49 @@
+const parameters = {
+  latitude: 25.20,
+  longitude: 55.27,
+  current_weather: "true",
+//   temperature_unit: "fahrenheit"
+};
+
+async function retrieveWeather(params) {
+  const url = `https://api.open-meteo.com/v1/forecast${params}`;
+
+//   try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      console.error(`Error received: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+//   } catch (error) {
+//     console.error("Error:", error.message);
+//   }
+}
+
+
+// composes a string of the format ?latitude=25.2&longitude=55.27&current_weather=true
+// from the parameters passed
+function composeParameters(params) {
+  let paramString = "";
+  for (const key in params) {
+    const separator = paramString.length === 0 ? "?" : "&";
+    paramString += separator + key + "=" + params[key];
+  }
+
+  return paramString;
+}
+
+// fetch the weather data
+async function fetchData() {
+//   try {
+    const params = composeParameters(parameters);
+    const data = await retrieveWeather(params);
+    console.log("Weather data:", data);
+//   } catch (error) {
+//     console.error("Failed to retrieve weather data:", error.message);
+//   }
+}
+
+fetchData();
